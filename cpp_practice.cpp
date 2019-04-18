26ad3e608c3758bac587fbe35377bb30039b2793
Ryan Barner <emailryanbarner@gmail.com>
2019-04-18 12:19:06 -0700

    Adding new problem

--- cpp_practice.cpp
+++ cpp_practice.cpp
@@ -4,3 +4,16 @@ int main() {
     std::cout<<"Hello world, I am ready for C++";
     return 0;
 }
+
+// Create a function that takes two numbers as arguments and return their sum.
+
+// Examples
+// addition(3, 2) ➞ 5
+
+// addition(-3, -6) ➞ -9
+
+// addition(7, 3) ➞ 10
+// Notes
+// Don't forget to return the result.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
